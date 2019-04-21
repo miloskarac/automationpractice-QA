5ea1c0233328fd7a4ca4eabe15568940efb82876
miloskarac <miloskarac@gmail.com>
2019-04-21 13:10:53 +0200

    [add] search.feature and search.steps

---
+++ AutomationPractice/Features/Search.feature
@@ -0,0 +1,10 @@
+﻿Feature: Search
+	in order to find product
+	As a user	
+	I want to be able to search for term
+
+@Search
+Scenario: User can search for term
+	Given user enters 'dress' search term
+	When user submits the search
+	Then result for a 'dress' search term are displayed
---
+++ AutomationPractice/SearchSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AutomationPractice
+{
+    [Binding]
+    public class SearchSteps
+    {
+        [Given(@"user enters '(.*)' search term")]
+        public void GivenUserEntersSearchTerm(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"user submits the search")]
+        public void WhenUserSubmitsTheSearch()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"result for a '(.*)' search term are displayed")]
+        public void ThenResultForASearchTermAreDisplayed(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
